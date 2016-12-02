c98639d70e2bf55f0735dd5da906614e4620e257
hienhpss <thanhhien008@gmail.com>
2016-12-02 14:21:51 +0800

    add users and avatars

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  get 'welcome/hello'
+  root to: "welcome#hello"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
