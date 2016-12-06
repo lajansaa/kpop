3348189fd8ad6944d0f5f61c362a9d3aa07d0351
Isa <lajansa@hotmail.com>
2016-12-06 21:18:24 +0800

    added resources for nomination to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get "api/fake_nominations" => "nominations#fake_nominations"
+
+  resources :nominations
 end
