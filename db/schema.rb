01c0440c9e0108170e518004d12fa8dec5c01cbf
hienhpss <thanhhien008@gmail.com>
2016-12-07 11:06:20 +0800

    replace new users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20161205155207) do
+ActiveRecord::Schema.define(version: 20161206020449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,16 +61,6 @@ ActiveRecord::Schema.define(version: 20161205155207) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "youtube_views", force: :cascade do |t|
-    t.date     "vote_start"
-    t.date     "vote_end"
-    t.string   "award"
-    t.string   "artiste"
-    t.string   "song"
-    t.bigint   "views"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -98,6 +87,17 @@ ActiveRecord::Schema.define(version: 20161205155207) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["fb_uid"], name: "index_users_on_fb_uid", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "youtube_views", force: :cascade do |t|
+    t.date     "vote_start"
+    t.date     "vote_end"
+    t.string   "award"
+    t.string   "artiste"
+    t.string   "song"
+    t.bigint   "views"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
