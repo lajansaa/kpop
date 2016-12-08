fa88f16ea6d1298109157ab4a85d1510256f3bc6
hienhpss <thanhhien008@gmail.com>
2016-12-08 23:23:46 +0800

    new root to awards, delete hello page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   resources :awards
-  get 'awards/index'
-
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'welcome/hello'
-  root to: "welcome#hello"
+  root to: "awards#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get "api/fake_nominations" => "nominations#fake_nominations"
-
   resources :nominations
 end
