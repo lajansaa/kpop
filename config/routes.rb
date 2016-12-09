251858703a16239ffb15f38c4d40d118df41fb72
Isa <lajansa@hotmail.com>
2016-12-09 23:59:17 +0800

    added api url

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'welcome/hello'
+  get 'api/v1/update_nominations' => 'nominations#nominations_fetcher'
   root to: "welcome#hello"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
