d8eb24a01b1c5d37362f1550f0ff6cfee52a1eb7
hienhpss <thanhhien008@gmail.com>
2016-12-12 16:25:41 +0800

    add styling for homepage

--- app/controllers/awards_controller.rb
+++ app/controllers/awards_controller.rb
@@ -4,7 +4,7 @@ class AwardsController < ApplicationController
   		sort_by = (params[:order] == 'name') ? 'name' : 'created_at'
   		@awards = Award.order(sort_by)
     elsif params[:keyword]
-      	@awards = Award.where( [ "name like ?", "%#{params[:keyword]}%" ] )
+      	@awards = Award.where( [ "lower(name) like ?", "%#{params[:keyword]}%" ] )
     else
       	@awards = Award.all
     end
