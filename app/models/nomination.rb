658983ecd902d154367fc426a59bf7c9a157484e
Isa <lajansa@hotmail.com>
2016-12-14 01:49:26 +0800

    declared relationship between award and nominations

--- app/models/nomination.rb
+++ app/models/nomination.rb
@@ -1,3 +1,6 @@
 class Nomination < ApplicationRecord
   acts_as_copy_target
+  belongs_to :award
+
+  #scope :for_week_of, -> (start_date) { where(vote_start: ) }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
   
   get 'api/v1/update_nominations' => 'nominations#update_nominations'
   get 'api/v1/get_nominations' => 'nominations#get_nominations'
-
+  get 'api/v1/get_artiste_info' => 'artistes#profile_img_fetcher'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :awards
+  resources :awards do
+    resources :nominations, :controller => "award_nominations"
+  end
   resources :nominations
 end
