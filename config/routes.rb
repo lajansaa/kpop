1da3919d749eadbc2834ec34b4055f11cdfd5a6b
Isa <lajansa@hotmail.com>
2016-12-16 01:42:30 +0800

    commented out update apis to prevent accidental updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ Rails.application.routes.draw do
   
   root to: "awards#index"
   
-  get 'api/v1/update_nominations' => 'nominations#update_nominations'
+  # get 'api/v1/update_nominations' => 'nominations#update_nominations'
   get 'api/v1/get_nominations' => 'nominations#get_nominations'
-  get 'api/v1/get_artiste_info' => 'artistes#profile_img_mnet'
-  get 'api/v2/get_artiste_info' => 'artistes#profile_img_kpop_scene'
-  get 'api/v1/get_thumbnails' => 'youtube_video_ids#thumbnail_img'
-  get 'api/v1/get_album_artiste' => 'albums#album_img'
+  # get 'api/v1/get_artiste_info' => 'artistes#profile_img_mnet'
+  # get 'api/v2/get_artiste_info' => 'artistes#profile_img_kpop_scene'
+  # get 'api/v1/get_thumbnails' => 'youtube_video_ids#thumbnail_img'
+  # get 'api/v1/get_album_artiste' => 'albums#album_img'
 
   get 'awards/:id/:vote_start/:vote_end/:order(.:format)' => 'awards#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
