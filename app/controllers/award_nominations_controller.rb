443422445c2bb0f4ba3093f8501a73a634eb2115
Isa <lajansa@hotmail.com>
2016-12-16 02:57:47 +0800

    added content and styling for each nomination

--- app/controllers/award_nominations_controller.rb
+++ app/controllers/award_nominations_controller.rb
@@ -3,6 +3,8 @@ class AwardNominationsController < ApplicationController
   def show
     @award = Award.find_by(id: params[:award_id])
     @nomination = Nomination.find_by(award_id: params[:award_id], id: params[:id]) 
+    @nomination_yv = YoutubeVideoId.find_by(artiste: @nomination.artiste, song: @nomination.song) 
+    @nomination_as = Album.find_by(artiste: @nomination.artiste, name: @nomination.album) 
   end
 
 end
