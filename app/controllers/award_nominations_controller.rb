0dba48ecfc39b688a920b99038b57e3381ac8b3e
Isa <lajansa@hotmail.com>
2016-12-16 02:10:13 +0800

    adjustments to width and margins

--- app/controllers/award_nominations_controller.rb
+++ app/controllers/award_nominations_controller.rb
@@ -1,6 +1,7 @@
 class AwardNominationsController < ApplicationController
 
   def show
+    @award = Award.find_by(id: params[:award_id])
     @nomination = Nomination.find_by(award_id: params[:award_id], id: params[:id]) 
   end
 
