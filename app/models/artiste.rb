e3d2333fb637e0ca0eee8e19f0fef1804a04d0e5
Isa <lajansa@hotmail.com>
2017-05-02 00:14:15 +0800

    declare one-to-many relation for artiste to songs

--- app/models/artiste.rb
+++ app/models/artiste.rb
@@ -1,4 +1,5 @@
 class Artiste < ApplicationRecord
   has_one :youtube_video
   has_many :nominees
+  has_many :songs
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ApplicationRecord
   belongs_to :album, optional: true
   has_one :youtube_video
   has_many :nominees
+  belongs_to :artiste
 end
