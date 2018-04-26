fa2f4adc43c00bffdd3bd938328f190bf046d4ec
Isa <lajansa@hotmail.com>
2018-04-26 12:57:18 +0800

    provided more info in json

--- app/controllers/api/v1/award_nominees_controller.rb
+++ app/controllers/api/v1/award_nominees_controller.rb
@@ -6,9 +6,50 @@ module Api::V1
       @award = Award.find(params[:award_id])
       @nominee = Nominee.find(params[:id])
       @provider_links = @nominee.song.digital_service_providers_links
+      
+      digital_service_providers = {}
+      @provider_links.each do |dsp|
+        digital_service_providers[dsp.digital_service_provider.name] = dsp.link
+      end
+
+      nominee = { artiste: { 
+                    name_eng: @nominee.artiste.name_eng,
+                    name_kor: @nominee.artiste.name_kor,
+                    profile_img: @nominee.artiste.profile_img
+                  },
+                  song: {
+                    name_eng: @nominee.song.name_eng,
+                    name_kor: @nominee.song.name_kor
+                  },
+                  ranking: {
+                    digital_sales: @nominee.mcountdown_ranking.normalized_ds,
+                    youtube_views: @nominee.mcountdown_ranking.normalized_yv,
+                    votes: @nominee.mcountdown_ranking.normalized_mv,
+                    remainder: @nominee.mcountdown_ranking.remainder,
+                    score: @nominee.mcountdown_ranking.score,
+                    ranking: @nominee.mcountdown_ranking.ranking
+                  },
+                  breakdown: {
+                    digital_sales: {
+                      download_cnt: @nominee.mcountdown_ranking.download_cnt,
+                      stream_cnt: @nominee.mcountdown_ranking.stream_cnt,
+                      digital_service_providers: digital_service_providers,
+                      profile_img: "https://ccmworld.files.wordpress.com/2013/07/gaon.jpg"
+                    },
+                    youtube_views: {
+                      video_present: @nominee.song.youtube_video.video_id == 'NA' ? false : true,
+                      view_cnt: @nominee.mcountdown_ranking.youtube_views,
+                      thumbnail_img: @nominee.song.youtube_video.thumbnail_img,
+                      video_link: @nominee.song.youtube_video.watch_link
+                    },
+                    mcountdown_votes: {
+                      votes: @nominee.mcountdown_ranking.mcountdown_votes,
+                      profile_img: "https://image.ibb.co/k9YaHb/m_countdown_logo_sgxclusive.png"
+                    }
+                  }
+                }
       render json: {award: @award,
-                    nominee: @nominee,
-                    provider_links: @provider_links
+                    nominee: nominee
                    }
     end
 
