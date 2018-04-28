499adc19a01e137d336a16b35cf45cde78ead826
Isa <lajansa@hotmail.com>
2018-04-29 00:56:27 +0800

    hard code user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,9 +7,10 @@ module Api::V1
       # @followings = TrackAwardNominee.where(:user_id => current_user.id)
       followings = []
       @followings.each do |f|
-        followings.push({ award_name: f.award.name,
+        followings.push({ award_id: f.award.id,
+                          award_name: f.award.name,
                           nomination_cycle: f.nominee.nomination_cycle.start_date.to_s + "~" + f.nominee.nomination_cycle.end_date.to_s,
-                          nomineed_id: f.nominee.id,
+                          nominee_id: f.nominee.id,
                           nominee_name: f.nominee.artiste.name_eng,
                           nominee_profile_img: f.nominee.artiste.profile_img,
                           song_name: f.nominee.song.name_eng,
