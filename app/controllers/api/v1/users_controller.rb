8450d6c4300701ccf1a4e7a31963e2a78e9c7cce
Isa <lajansa@hotmail.com>
2018-04-28 16:33:59 +0800

    added nominee id

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,15 @@
 module Api::V1
   class UsersController < ApplicationController
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
 
     def following
+      # @followings = TrackAwardNominee.where(:user_id => 1)
       @followings = TrackAwardNominee.where(:user_id => current_user.id)
       followings = []
       @followings.each do |f|
         followings.push({ award_name: f.award.name,
                           nomination_cycle: f.nominee.nomination_cycle.start_date.to_s + "~" + f.nominee.nomination_cycle.end_date.to_s,
+                          nomineed_id: f.nominee.id,
                           nominee_name: f.nominee.artiste.name_eng,
                           nominee_profile_img: f.nominee.artiste.profile_img,
                           song_name: f.nominee.song.name_eng,
@@ -17,6 +19,9 @@ module Api::V1
       render json: { user_name: current_user.name,
                      followings: followings
                    }
+      # render json: { user_name: 'Isa',
+      #                followings: followings
+      #              }
     end
   end
 end
